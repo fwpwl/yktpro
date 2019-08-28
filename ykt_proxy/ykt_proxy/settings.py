ead5d6f421224f8ead68d38677431f5c7d5a259b
changping <2623116435@qq.com>
2019-08-28 14:16:27 +0800

    MOD:update

--- ykt_proxy/ykt_proxy/settings.py
+++ ykt_proxy/ykt_proxy/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
 ]
 ROOT_URLCONF = 'ykt_proxy.urls'
 
