fc53c730af407775dd244722eb4e1c531b6cc3e7
fwp <fwp@myhost.local>
2020-02-11 19:50:49 +0800

    update

--- ykt_proxy/data_transfer/module_managers/tjcu_manager.py
+++ ykt_proxy/data_transfer/module_managers/tjcu_manager.py
@@ -29,7 +29,7 @@ def get_client(func):
         conn = MySQLTransferHandler(host="127.0.0.1",
                                     port=3306,
                                     user="root",
-                                    password="Ykt2019",
+                                    password="Ykt2020$",
                                     database="ykt")
         return func(conn)
 
