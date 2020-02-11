dcf74331f4b0a904a5595a1b768482b7377a3dd7
fwp <fwp@myhost.local>
2020-02-11 20:08:38 +0800

    update

--- ykt_proxy/data_transfer/module_views/tjcu_views.py
+++ ykt_proxy/data_transfer/module_views/tjcu_views.py
@@ -13,9 +13,9 @@ def tjcu_get_department_data_view(request):
     """
     URL[GET]:/data/tjcu/tjcu_get_department_data/
     """
-    key = get_para_from_request_safe(request, 'key')
-    if not is_valid_request(key):
-        return error_response('无效的请求!')
+    # key = get_para_from_request_safe(request, 'key')
+    # if not is_valid_request(key):
+    #     return error_response('无效的请求!')
 
     ret_data = tjcu_get_department_data()
     return success_response(ret_data)
@@ -25,9 +25,9 @@ def tjcu_get_tra_classroom_data_view(request):
     """
     URL[GET]:/data/tjcu/tjcu_get_tra_classroom_data/
     """
-    key = get_para_from_request_safe(request, 'key')
-    if not is_valid_request(key):
-        return error_response('无效的请求!')
+    # key = get_para_from_request_safe(request, 'key')
+    # if not is_valid_request(key):
+    #     return error_response('无效的请求!')
 
     ret_data = tjcu_get_tra_classroom_data()
     return success_response(ret_data)
@@ -37,9 +37,9 @@ def tjcu_get_user_data_view(request):
     """
     URL[GET]:/data/tjcu/tjcu_get_user_data/
     """
-    key = get_para_from_request_safe(request, 'key')
-    if not is_valid_request(key):
-        return error_response('无效的请求!')
+    # key = get_para_from_request_safe(request, 'key')
+    # if not is_valid_request(key):
+    #     return error_response('无效的请求!')
 
     ret_data = tjcu_get_user_data()
     return success_response(ret_data)
@@ -49,9 +49,9 @@ def tjcu_get_course_data_view(request):
     """
     URL[GET]:/data/tjcu/tjcu_get_course_data/
     """
-    key = get_para_from_request_safe(request, 'key')
-    if not is_valid_request(key):
-        return error_response('无效的请求!')
+    # key = get_para_from_request_safe(request, 'key')
+    # if not is_valid_request(key):
+    #     return error_response('无效的请求!')
 
     ret_data = tjcu_get_course_data()
     return success_response(ret_data)
@@ -61,9 +61,9 @@ def tjcu_get_choose_data_view(request):
     """
     URL[GET]:/data/tjcu/tjcu_get_choose_data/
     """
-    key = get_para_from_request_safe(request, 'key')
-    if not is_valid_request(key):
-        return error_response('无效的请求!')
+    # key = get_para_from_request_safe(request, 'key')
+    # if not is_valid_request(key):
+    #     return error_response('无效的请求!')
 
     ret_data = tjcu_get_choose_data()
     return success_response(ret_data)
