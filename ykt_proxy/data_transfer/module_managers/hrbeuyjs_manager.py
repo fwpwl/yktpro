5eaf586e656a76d0d86e6ead80a9ea4d211dbc93
fwp <fwp@myhost.local>
2020-04-27 10:30:50 +0800

    fix bug

--- ykt_proxy/data_transfer/module_managers/hrbeuyjs_manager.py
+++ ykt_proxy/data_transfer/module_managers/hrbeuyjs_manager.py
@@ -5,7 +5,8 @@ from data_transfer.utils.datetime_utils import get_now_datetime_str, FORMAT_DATE
 import pymysql
 
 
-# db = pymysql.connect(host=host, port=port, user=user, passwd=password, db=database)
+db = pymysql.connect(host="192.168.21.13", port=3306, user="root", passwd="Ykt@2020", db="ykt")
+cursor = db.cursor()
 
 
 def is_valid_request(key):
@@ -49,11 +50,11 @@ def query_data_to_dict_list(query_data_list_of_tuple, keys_list):
 def hrbeuyjs_get_department_data():
     print('start request department')
     statement = "select xymc from xyxxb"
-    data_list = get_db_client()
-    print('data_list', data_list)
-    keys_list = ["department_name"]
-    final_info_list = query_data_to_dict_list(data_list, keys_list)
-    return final_info_list
+    cursor.execute(statement)
+    data_list = cursor.fetchall()
+    # final_info_list = query_data_to_dict_list(data_list, keys_list)
+    print(data_list)
+    return data_list
 
 def hrbeuyjs_get_tra_data():
     statement = "select ssxy, ZY, bjmc, rxxn from xzbjb"
