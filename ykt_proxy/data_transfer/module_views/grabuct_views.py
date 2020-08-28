9a0910f2f8854c9f7aa77d330b66160b3f505f9e
fwp <fwp@myhost.local>
2020-08-28 12:45:30 +0800

    fix bug

--- ykt_proxy/data_transfer/module_views/grabuct_views.py
+++ ykt_proxy/data_transfer/module_views/grabuct_views.py
@@ -13,9 +13,9 @@ def grabuct_get_department_data_view(request):
     """
     URL[GET]:/data/grabuct/grabuct_get_department_data/
     """
-    # key = get_para_from_request_safe(request, 'key')
-    # if not is_valid_request(key):
-    #     return error_response('无效的请求!')
+    key = get_para_from_request_safe(request, 'key')
+    if not is_valid_request(key):
+        return error_response('无效的请求!')
 
     ret_data = grabuct_get_department_data()
     return success_response(ret_data)
@@ -25,9 +25,9 @@ def grabuct_get_tra_classroom_data_view(request):
     """
     URL[GET]:/data/grabuct/grabuct_get_tra_classroom_data/
     """
-    # key = get_para_from_request_safe(request, 'key')
-    # if not is_valid_request(key):
-    #     return error_response('无效的请求!')
+    key = get_para_from_request_safe(request, 'key')
+    if not is_valid_request(key):
+        return error_response('无效的请求!')
 
     ret_data = grabuct_get_tra_classroom_data()
     return success_response(ret_data)
@@ -37,9 +37,9 @@ def grabuct_get_user_data_view(request):
     """
     URL[GET]:/data/grabuct/grabuct_get_user_data/
     """
-    # key = get_para_from_request_safe(request, 'key')
-    # if not is_valid_request(key):
-    #     return error_response('无效的请求!')
+    key = get_para_from_request_safe(request, 'key')
+    if not is_valid_request(key):
+        return error_response('无效的请求!')
 
     ret_data = grabuct_get_user_data()
     return success_response(ret_data)
@@ -49,9 +49,9 @@ def grabuct_get_course_data_view(request):
     """
     URL[GET]:/data/grabuct/grabuct_get_course_data/
     """
-    # key = get_para_from_request_safe(request, 'key')
-    # if not is_valid_request(key):
-    #     return error_response('无效的请求!')
+    key = get_para_from_request_safe(request, 'key')
+    if not is_valid_request(key):
+        return error_response('无效的请求!')
     year = get_para_from_request_safe(request, 'year')
     term = get_para_from_request_safe(request, 'term')
 
@@ -63,9 +63,9 @@ def grabuct_get_choose_data_view(request):
     """
     URL[GET]:/data/grabuct/grabuct_get_choose_data/
     """
-    # key = get_para_from_request_safe(request, 'key')
-    # if not is_valid_request(key):
-    #     return error_response('无效的请求!')
+    key = get_para_from_request_safe(request, 'key')
+    if not is_valid_request(key):
+        return error_response('无效的请求!')
     year = get_para_from_request_safe(request, 'year')
     term = get_para_from_request_safe(request, 'term')
     ret_data = grabuct_get_choose_data(year, term)
